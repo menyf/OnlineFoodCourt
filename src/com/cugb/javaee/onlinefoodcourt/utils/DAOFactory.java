36eb48f02d9001423bff524186db86992a127db6
menyf <304078150@qq.com>
2017-06-06 11:07:20 +0800

    the latest

---
+++ src/com/cugb/javaee/onlinefoodcourt/utils/DAOFactory.java
@@ -0,0 +1,29 @@
+package com.cugb.javaee.onlinefoodcourt.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class DAOFactory {
+	private static Properties prop = null;
+	
+	private DAOFactory() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static{
+		InputStream in = DAOFactory.class.getClassLoader().getResourceAsStream("dao.properties");
+		prop = new Properties();
+		try {
+			prop.load(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object newInstance(String interfaceClassName) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+		String classname = prop.getProperty(interfaceClassName);
+		return Class.forName(classname).newInstance();
+	}
+}
